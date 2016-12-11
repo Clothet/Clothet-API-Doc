6e624f70cd7abc24dd1319b37f21b50ff9063d09
ssarcandy <ssarcandy@gmail.com>
2016-12-11 19:48:42 +0000

    update

--- doc/api_project.js
+++ doc/api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-12-11T19:44:58.164Z",
+    "time": "2016-12-11T19:48:42.130Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
