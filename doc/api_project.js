b4a3a3094ed64fcf59b93344147edbe00c6f0bbe
ssarcandy <ssarcandy@gmail.com>
2016-12-11 21:57:14 +0800

    init

---
+++ doc/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "Clothet-API",
+  "version": "0.0.1",
+  "description": "document for Clothet-API",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-12-11T13:56:17.080Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
